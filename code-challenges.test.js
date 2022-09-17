2eeaac6d4dda49daadf31bb7ef3aced3b33c87f4
ZackRohde <zdrohde@outlook.com>
2022-09-17 15:11:23 -0700

    Coding Challenges Complete 1/3

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -29,13 +29,13 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-// describe("divisibleByThree", () => {
-//   it("decides if the number inside it is evenly divisible by three or not.", () => {
-//     expect(divisibleByThree(object1)).toEqual("15 is divisible by three")
-//     expect(divisibleByThree(object2)).toEqual("0 is divisible by three")
-//     expect(divisibleByThree(object3)).toEqual("-7 is not divisible by three")
-//   })
-// })
+describe("divisibleByThree", () => {
+  it("decides if the number inside it is evenly divisible by three or not.", () => {
+    expect(divisibleByThree(object1)).toEqual("15 is divisible by three")
+    expect(divisibleByThree(object2)).toEqual("0 is divisible by three")
+    expect(divisibleByThree(object3)).toEqual("-7 is not divisible by three")
+  })
+})
 
 // ReferenceError: divisibleByThree is not defined
 
@@ -48,7 +48,7 @@ const object3 = { number: -7 }
 
 // b) Create the function that makes the test pass.
 
-// PSUEDO CODE
+// PSUEDO CODE:
 // Create a function with the function name of divisibleByThree
 // Only one parameter (object)
 // Make a conditional statement checking to see if the value is divisble by three
@@ -79,7 +79,7 @@ describe("capWords", () => {
   })
 })
 
-// ReferenceError: capWords is not defined
+// // ReferenceError: capWords is not defined
 
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
@@ -90,20 +90,33 @@ const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 
 // PSUEDO CODE:
 // Create a function named capWords
-// Create an empty array 
 // Iterate through the array using .map method
-// In order to capitalize each first character I'll use the .toUpperCase method
+// In order to capitalize each first character I'll need to use charAt(0) which means it's going over only the first character of the index
+// Then I'll use .toUpperCase() to capitalize each first character
+// After I'll need it to stop so I can use .slice(1) which means it will start at the first index character and then using .toLowerCase() ensures everything after the first index is lowercased
 
-const capWords = () => {
-  let newArr = []
-
-  return value.charAt(0).toUpperCase() + element.slice(1).toLowerCase()
+function capWords(newArr) {
+  return newArr.map(element => {
+    return element.charAt(0).toUpperCase() + element.slice(1).toLowerCase()
+  })
 }
 
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("firstVowel", () => {
+  it("takes in a string and logs the index of the first vowel.", () => {
+    expect(firstVowel(vowelTester1)).toEqual(1)
+    expect(firstVowel(vowelTester2)).toEqual(0)
+    expect(firstVowel(vowelTester3)).toEqual(2)
+  })
+})
+
+// ReferenceError: firstVowel is not defined
 
 const vowelTester1 = "learn"
 // Expected output: 1
@@ -113,3 +126,24 @@ const vowelTester3 = "challenges"
 // Expected output: 2
 
 // b) Create the function that makes the test pass.
+
+// PSUEDO CODE:
+// Create a function, no name given so I named it firstVowel
+// Create a forloop that says the index is less than the firstVowels length
+// Create a conditional statement that goes through each index of the string and checks if it's a vowel and if it is in it will continue until the vowel is found which means using the logical operator OR || 
+// Create a return statetment of i
+
+const firstVowel = (str) => {
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "i" ||
+      str[i] === "o" ||
+      str[i] === "u") {
+      return i
+    }
+  }
+}
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
